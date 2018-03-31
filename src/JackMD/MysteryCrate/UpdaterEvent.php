d233102190b4d7b7f6500169de95f878de837542
iZeaoGamer <jameshayward14@icloud.com>
2018-03-31 19:56:38 +0100

    Update UpdaterEvent.php

--- src/JackMD/MysteryCrate/UpdaterEvent.php
+++ src/JackMD/MysteryCrate/UpdaterEvent.php
@@ -106,12 +106,12 @@ class UpdaterEvent extends PluginTask
 
                     $slot13 = $this->chest->getInventory()->getItem(13);
                     $player = $this->player;
+                    $playerName = $player->getName();
                     
-                    if ($player instanceof Player) {
-                        $player = $this->player;
+                    if ($playerName instanceof Player) {
                         $this->player->getInventory()->addItem($slot13);
                         $this->player->sendMessage(TextFormat::GREEN . "You recieved " . TextFormat::YELLOW . $slot13->getName() . TextFormat::LIGHT_PURPLE . " (x" . $slot13->getCount() . ")" . TextFormat::GREEN . " from " . $this->plugin->crateName);
-                        $this->plugin->getServer()->broadcastMessage(TextFormat::AQUA . "$player " . TextFormat::YELLOW . "§ajust opened §bMysteryCrate §cand got: §3$slot13");
+                        $this->plugin->getServer()->broadcastMessage(TextFormat::AQUA . "$player->getName() " . TextFormat::YELLOW . "§ajust opened §bMysteryCrate §cand got: §3$slot13");
                     }
 
                     $this->plugin->getServer()->getScheduler()->scheduleDelayedTask(new RemoveChest($this->plugin, $cpos), 20);
